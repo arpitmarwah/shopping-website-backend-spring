292d3a57309450aef224bc74d517515039318207
Arpit Marwah <arpit.marwah19@gmail.com>
2023-12-15 11:56:31 +0530

    first commit

---
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -0,0 +1,61 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.InventoryResponse;
+import com.example.orderservice.dto.OrderLineItemsDto;
+import com.example.orderservice.dto.OrderRequest;
+import com.example.orderservice.model.Order;
+import com.example.orderservice.model.OrderLineItems;
+import com.example.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToOrder)
+                .toList();
+        order.setOrderLineItemsList(orderLineItems);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray).allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock) {
+            orderRepository.save(order);
+        }
+        else  {
+            throw new IllegalArgumentException("Product not in stock");
+        }
+    }
+
+    private OrderLineItems mapToOrder(OrderLineItemsDto orderLineItemDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemDto.getSkuCode());
+        return orderLineItems;
+    }
+}
